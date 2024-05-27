04320944c63dc9f54cbdd815a9ab0fe69977c035
Aaron Thomas <thomasattraction@yahoo.com>
2024-05-27 19:44:03 +0100

    new gustebook!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/pages/Navbar';
+import Homepage from './components/pages/Homepage';
+import About from './components/pages/About';
+import Contacts from './components/pages/Contacts';
+import Form from './components/pages/Form';
+import Playlist from './components/pages/Playlist';
 
+/*Functional Component*/
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return (  
+<div className="Nav">  
+      <Router>
+      <header className="">
+      <Navbar /> 
+      </header>   
+       <Routes>
+        <Route exact path = "/" element = {<Homepage />} />
+        <Route exact path = "/About" element = {<About />} />
+        <Route exact path = "/Contacts" element = {<Contacts />} />
+        <Route exact path = "/Form" element = {<Form />} />
+        <Route exact path = "/Playlist" element = {<Playlist />} />
+       </Routes>
+      </Router>
     </div>
+   
   );
 }
 
